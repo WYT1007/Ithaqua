e814e7a85c98a5e0ef868835165feb0473ef7d28
WYT1007 <2500455273@qq.com>
2024-06-12 11:00:57 +0800

    Update demo-basic-operations.py

--- demo-basic-operations.py
+++ demo-basic-operations.py
@@ -4,7 +4,7 @@ st.header("Lord of Babel, King of the Tower")
 st.subheader("His real name, perhaps the only person in the world who knows it, can no longer call out his voice, and that doesn't matter to him anymore.")
 st.text("My dear brother, watch how the ants under your feet break free from their shackles and bring down the sun.")
 
-st.markdown("this is an image:https://image.baidu.com/search/detail?ct=503316480&z=0&ipn=d&word=%E4%BC%8A%E5%A1%94%E5%BA%93%E4%BA%9A&hs=0&pn=1&spn=0&di=7360350738658099201&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&ie=utf-8&oe=utf-8&cl=2&lm=-1&cs=2049723241%2C3858745494&os=4011166354%2C3849134431&simid=3480176788%2C394560162&adpicid=0&lpn=0&ln=30&fr=ala&fm=&sme=&cg=&bdtype=0&oriquery=%E4%BC%8A%E5%A1%94%E5%BA%93%E4%BA%9A&objurl=https%3A%2F%2Fb0.bdstatic.com%2F4b64cc74f52dfa56cde28d7cd8043bb0.jpg%40h_1280&fromurl=ipprf_z2C%24qAzdH3FAzdH3F4k1_z%26e3Bkwt17_z%26e3Bv54AzdH3Fgjofrw2jAzdH3F1wpwAzdH3F1pswg1tg2otfj%3Fgt1%3D1p_ca08b9amdl88mdmnd08%26f576vjF654%3Di54jrw2j&gsm=&islist=&querylist=&dyTabStr=MCwyLDMsMSw2LDQsNSw3LDgsOQ%3D%3D")
+st.markdown("this is an image:https://b0.bdstatic.com/4b64cc74f52dfa56cde28d7cd8043bb0.jpg@h_1280")
 
 if st.checkbox("Show/Hide"):
     st.text("You checked the box")
